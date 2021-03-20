b3feaa365bb10364728add4d6a877e9b344acb4b
Rio Arjuna <ri0arjuna@programmer.net>
2021-03-20 21:56:05 +0700

    create new activity_create_note.xml

---
+++ app/src/main/java/rio/arj/inotes/create/CreateNoteActivity.kt
@@ -0,0 +1,12 @@
+package rio.arj.inotes.create
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import rio.arj.inotes.R
+
+class CreateNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
