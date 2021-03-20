ccc13fc705cd82f90b88ec7e05ab3b638e8b4f00
Rio Arjuna <ri0arjuna@programmer.net>
2021-03-20 22:36:12 +0700

    create unit test CreateNoteModelTest.kt

---
+++ app/src/main/java/rio/arj/inotes/repository/model/CreateNoteModel.kt
@@ -0,0 +1,20 @@
+package rio.arj.inotes.repository.model
+
+data class CreateNoteModel(
+    val title: String,
+    val content: String,
+    val dateCreated: String = System.currentTimeMillis().toString()
+) {
+    fun isTitleMaxLength(): Boolean {
+        return title.length >= 50
+    }
+
+    fun isContentMaxLength(): Boolean {
+        return content.length >= 2500
+    }
+
+    fun isValidToSave(): Boolean {
+        return !isTitleMaxLength() && !isContentMaxLength()
+    }
+
+}
