15d075e8adf0f9531ebb4743efe952481019e56b
Rio Arjuna <ri0arjuna@programmer.net>
2021-04-11 20:01:22 +0700

    link activity_main.xml to activity_create_note.xml

---
+++ app/src/main/java/rio/arj/inotes/Constants.kt
@@ -0,0 +1,7 @@
+package rio.arj.inotes
+
+object Constants {
+
+  const val REQUEST_CODE_CREATE_NEW_NOTES = 1
+
+}
--- app/src/main/java/rio/arj/inotes/features/main/MainActivity.kt
+++ app/src/main/java/rio/arj/inotes/features/main/MainActivity.kt
@@ -1,12 +1,30 @@
 package rio.arj.inotes.features.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import rio.arj.inotes.Constants.REQUEST_CODE_CREATE_NEW_NOTES
 import rio.arj.inotes.R
+import rio.arj.inotes.databinding.ActivityMainBinding
+import rio.arj.inotes.features.create.CreateNoteActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+  private lateinit var binding: ActivityMainBinding
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+    listener()
+  }
+
+  private fun listener() {
+    binding.buttonCreateNewNote.setOnClickListener {
+      val intent = Intent(this, CreateNoteActivity::class.java)
+      startActivityForResult(intent, REQUEST_CODE_CREATE_NEW_NOTES)
     }
+  }
 }
